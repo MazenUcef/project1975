f975dea4574f5d9da173be7f1e88c6ddc97d8c6a
MazenY <mazenafifi1999@gmail.com>
2023-12-30 19:24:24 +0200

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
 import express from 'express'
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
+import userRouter from './routes/user.route.js';
+// import dotenv from 'dotenv';
+// dotenv.config();
 
+// server listen
+const app = express();
+app.listen(5000 , ()=>{
+    console.log('Server is running in port 5000 ');
+})
+
+// data base connection
 mongoose
 .connect("mongodb+srv://mazenucef:mazenucef@project-estate.usg1dux.mongodb.net/project-estate?retryWrites=true&w=majority")
 .then(()=>{
@@ -11,10 +19,12 @@ mongoose
 .catch((err)=>{
     console.log(err);
 })
+// =====================================================================
 
-const app = express();
-app.listen(5000 , ()=>{
-    console.log('Server is running in port 5000 ');
-})
+// api route
+app.use("/api/user" , userRouter)
+
+
+
 
 
