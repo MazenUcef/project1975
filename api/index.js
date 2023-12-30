96a1f78aa84a2ffe97b4462cb39feb717a03c60f
MazenY <mazenafifi1999@gmail.com>
2023-12-30 18:27:46 +0200

    connect to DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+.connect("mongodb+srv://mazenucef:mazenucef@project-estate.usg1dux.mongodb.net/project-estate?retryWrites=true&w=majority")
+.then(()=>{
+    console.log("connected to MongooDB");
+})
+.catch((err)=>{
+    console.log(err);
+})
 
 const app = express();
 app.listen(5000 , ()=>{
     console.log('Server is running in port 5000 ');
 })
+
 
