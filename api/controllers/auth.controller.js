ac76733616be58324c5b44b54e747d15bb0f5ffc
MazenY <mazenafifi1999@gmail.com>
2023-12-31 02:39:15 +0200

    sign in route creation

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,11 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
+
+
 
 export const signup = async (req , res ,next)=>{
-    const {username  ,email , password} = req.body;
+    const {username  , email , password} = req.body;
     const hashedPass = bcryptjs.hashSync(password , 10);
     const newUser = new User({username , email ,password:hashedPass});
     try {
@@ -11,5 +14,20 @@ export const signup = async (req , res ,next)=>{
     } catch (error) {
         next(error)
     }
-
 }
+
+export const signin = async (req , res , next)=>{
+const {email , password} = req.body;
+try {
+    const validUser = await User.findOne({email});
+    if(!validUser) return next(errorHandler(404,'User not found'))
+    const validPassword = bcryptjs.compareSync(password ,validUser.password)
+if(!validPassword) return next(errorHandler(401,'Wrong credentials!'))
+const {password : pass,...rest} = validUser._doc
+res
+.status(200)
+.json(rest)
+} catch (error) {
+    next(error)
+}
+};
